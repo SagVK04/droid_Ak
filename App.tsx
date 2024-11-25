7e2bd383ca0bd9647c329e6f88f582e349b01ac0
Sagnik <metiyasagnik692@gmail.com>
2024-11-25 22:45:25 +0530

    Updated with website linking & JSON Stringify methods

--- App.tsx
+++ App.tsx
@@ -1,118 +1,154 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React,{Component, useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  Text, View, FlatList, Button, StyleSheet,TextInput,ScrollView,
+  SectionList, Modal, Pressable, StatusBar,
+  Platform,Linking} from 'react-native';
+import {WebView} from "react-native-webview";
+const App = () =>{
+  const [showmodal,setShowmodal] = useState(false);
+  const [showopen,setShowopen] = useState(true);
+  const [statusBar,showStatusBar] = useState(false);
+  const openButton = (()=>{
+    setShowmodal(true); setShowopen(false);
+  })
+  const closeButton = (()=>{
+    setShowmodal(false); setShowopen(true);
+  })
+  const setStatusbar = (()=>{
+    showStatusBar(false);
+  })
+  const remStatusBar = (()=>{
+    showStatusBar(true);
+  })
+  const openWeb = (()=>{
+      Linking.openURL("https://www.youtube.com/")
+  })
+  const openWeb1 = (()=>{
+    Linking.openURL("https://www.facebook.com/")
+  })
+  const openWeb2 = (()=>{
+    Linking.openURL("https://www.instagram.com/")
+  })
+  const openWeb3 = (()=>{
+    Linking.openURL("https://play.google.com/store/apps?hl=en_IN&pli=1")
+  })
+  return(
+    <View style={styles.main}>
+      {
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" hidden={statusBar}></StatusBar>
+      }
+      <Modal transparent={true} visible={showmodal} animationType='slide'>
+        <View style={styles.centeredview}>
+          <View style={styles.modalview}>
+            <Text style={[{fontSize:30},{marginBottom:15},{color:'black'},{marginLeft:20}]}>Hello!</Text>
+            <Button title='Close Modal' onPress={closeButton}></Button>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </Modal>
+      
+      <View style={styles.text}>
+        <Pressable style={styles.press} 
+          //onPress={()=>console.warn("Normal on press")}
+          //onLongPress={()=>console.warn("This is long press")} [Delay 0.5 second by default]
+          onPressIn={()=>console.log("Onpress In")}
+          onPressOut={()=>console.log("Onpress Out")}
+          onLongPress={()=>console.warn("This is long press")}
+          delayLongPress={3000} //for delaying long press
+        >
+          <Text style={[{fontSize:25},{color:'red'}]}>Pressable</Text>
+        </Pressable>
+        <View style={styles.inner}>
+          <View style={styles.inner1}>
+            <Text style={[{fontSize:30},{color:"black"}]}>Dialog Box</Text>
+          </View>
+        </View>
+        <View>
+          <Text style={[{fontSize:20},{color:"black"},{marginTop:10}]}>Platform: {Platform.OS}</Text>
+          {
+            Platform.OS=="ios"?
+            <View style={styles.inner1a}>
+              <Text style={styles.textPl}>This is {Platform.OS} Platform</Text>
+            </View>
+            :
+            <View style={styles.inner1a}>
+              <Text style={styles.textPl}>This is {Platform.OS} Platform</Text>
+            </View>
+          }
+        </View>
+        <View style={{marginTop:10}}>
+          <Button title='Open Youtube' onPress={openWeb}></Button>
+          <View style={{marginTop:15}}>
+            <Button title='Open Facebook' onPress={openWeb1}></Button>
+          </View>
+          <View style={{marginTop:15}}>
+            <Button title='Open Instagram' onPress={openWeb2}></Button>
+          </View>
+          <View style={{marginTop:15}}>
+            <Button title='Open Play Store' onPress={openWeb3}></Button>
+          </View>
+        </View>
+      </View>
+      <Text style={{fontSize:19,color:"black"}}>
+        {JSON.stringify(Platform.constants.reactNativeVersion)}
+        {JSON.stringify(Platform.constants.reactNativeVersion.major)},
+        {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
+      </Text>
+      <View style={styles.buttonview}>
+        { showopen ? <Button title='Open modal' onPress={openButton}></Button> : null }
+      </View>
+      <View style={styles.buttonview1}>
+        {
+          !statusBar ? 
+          <Button title='Hide StatusBar' onPress={remStatusBar}></Button> 
+          : 
+          <Button title='Show StatusBar' onPress={setStatusbar}></Button>
+        }
+      </View>
+    </View>
+  )
 }
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  main:{
+      flex: 1, backgroundColor: '#fff',
+      justifyContent:'center', alignItems:'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  buttonview:{
+    flex:1, justifyContent:'flex-end', marginBottom:7
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  buttonview1:{
+    flex:0.3, justifyContent:'flex-end',shadowColor:"black",elevation:5,marginBottom:11
   },
-  highlight: {
-    fontWeight: '700',
+  text:{
+    flex:5, justifyContent:'center'
   },
-});
-
+  textModified:{
+    flex:2.5, justifyContent:'center'
+  },
+  textPl:{
+    color: Platform.OS=="android"? "black" : "blue",
+    fontSize: 18
+  },
+  inner:{
+     borderColor:'red', borderWidth:4, padding:3,borderRadius:6
+  },
+  inner1:{
+    borderColor:'red', borderWidth:4, padding:17,borderRadius:6, paddingLeft:42
+  },
+  inner1a:{
+    borderColor:'maroon', borderWidth:4, padding:17,borderRadius:6, 
+    paddingLeft:21,marginTop:10,
+  },
+  centeredview:{
+    flex:1,justifyContent:'center',alignItems:'center'
+  },
+  modalview:{
+    backgroundColor: "#fff", color:"black",padding:28,marginTop:50,
+    borderRadius:20,shadowColor:"black",elevation:8,
+  },
+  press:{
+    justifyContent:'center',alignItems:'center',backgroundColor:"#ffa", shadowColor:"black",
+    marginBottom:17, borderRadius:4, width:120,marginLeft:"auto",marginRight:"auto",
+    elevation:10
+  }
+}) 
 export default App;
