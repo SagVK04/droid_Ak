eaa33b783e123648a07276efbbff5672b96fb363
Sagnik <metiyasagnik692@gmail.com>
2025-01-19 02:07:57 +0530

    Added with no result foud & clear input

--- API Searching/App.tsx
+++ API Searching/App.tsx
@@ -8,6 +8,7 @@ import {WebView} from "react-native-webview";
 const App = () =>{
   const [showmodal,setShowmodal] = useState(false);
   const [statusBar,showStatusBar] = useState(false);
+  const [name,setName] = useState("");
   const openButton = (()=>{
     setShowmodal(true);
   })
@@ -32,16 +33,27 @@ const App = () =>{
   const openWeb3 = (()=>{
     Linking.openURL("https://play.google.com/store/apps?hl=en_IN&pli=1")
   })
+  const openWeb5 = (()=>{
+    Linking.openURL("https://google.com/")
+  })
   const [data,setData] = useState([]);
   const openWeb4 = async (text)=>{
-    const url=`http://192.168.0.184:3000/users?q=${text}`;
-    //console.warn(url);
-    let result=await fetch(url);
-    let result1=await result.json();
-    if(result1){
-      setData(result1);
+    if(text != null){
+      setName(text);
+      const url=`http://192.168.0.184:3000/users?q=${text}`;
+      //console.warn(url);
+      let result=await fetch(url);
+      let result1=await result.json();
+      if(result1)
+        setData(result1);
+    }
+    else{
+      setName(" ");
     }
   }
+  const openWeb6 = (()=>{
+    
+  })
   return(
     <View style={styles.main}>
       <View style={styles.text1}> 
@@ -50,9 +62,12 @@ const App = () =>{
         </Text>  
       </View>
       <View style={styles.input1}>
-        <TextInput style={styles.input2} onChangeText={(text)=>openWeb4(text)}></TextInput>
-        <TouchableHighlight style={styles.text1a}>
-          <Text style={{marginTop:"auto",marginBottom:"auto"}}>Search Google</Text>
+        <TextInput style={styles.input2} placeholder='Enter your query' value={name} onChangeText={(text)=>openWeb4(text)}></TextInput>
+        <TouchableHighlight style={styles.text1aa} onPress={openWeb4}>
+          <Text style={{marginTop:"auto",marginBottom:"auto"}}>Clear</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.text1a} onPress={openWeb5}>
+          <Text style={{marginTop:"auto",marginBottom:"auto"}}>Open Google</Text>
         </TouchableHighlight>
         <View style={{flex:1}}>
         {
@@ -62,7 +77,14 @@ const App = () =>{
               <Text>{item.age}</Text>
               <Text>{item.id}</Text>
             </View>)
-            :null
+            :
+            <View>{
+              name.length ?
+                <Text>No result found!</Text>
+              :
+                null
+              }
+              </View>
         }
         </View>
         <Text></Text>
@@ -72,7 +94,7 @@ const App = () =>{
 }
 const styles = StyleSheet.create({
   main:{
-      flex:1,backgroundColor: 'white',
+      flex:1,backgroundColor: 'greenblue',
       justifyContent:"center", alignItems:'center',
   },
   main1:{
@@ -89,6 +111,11 @@ const styles = StyleSheet.create({
   },
   text1a:{
     alignItems:"center", backgroundColor:"skyblue",marginTop:10,
+    borderWidth:1, marginRight:2, marginLeft:2,
+    width:125,height:40,borderRadius:12.5,marginBottom:"auto",top:-3,left:90,
+  },
+  text1aa:{
+    alignItems:"center", backgroundColor:"red",marginTop:10,
     borderWidth:1, marginRight:2, marginLeft:2,
     width:125,height:40,borderRadius:12.5,marginBottom:"auto",top:-3,left:90,
   },
